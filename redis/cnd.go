18c7eca4562be9291b22ca3b05d20d4d9d478f33
fengyang <99>
2023-10-17 16:57:55 +0800

    init

--- redis/cnd.go
+++ redis/cnd.go
@@ -16,7 +16,7 @@ var (
 )
 
 type Conf struct {
-	Host     string `mapstructure:"hots" json:"hots" yaml:"hots"`
+	Host     string `mapstructure:"host" json:"host" yaml:"host"`
 	Port     string `mapstructure:"port" json:"port" yaml:"port"`
 	Password string `mapstructure:"password" json:"password" yaml:"password"`
 	DB       int    `mapstructure:"db" json:"db" yaml:"db"`
